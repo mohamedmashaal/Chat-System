9d4a79e19a164bd3e266bd18f7441bf488483854
MohamedMashaal <mohamedmashaal1997@gmail.com>
2022-04-14 14:49:41 +0200

    minor validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+  validates :number, uniqueness: { scope: :chat_id }
   belongs_to :chat, counter_cache: true
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
